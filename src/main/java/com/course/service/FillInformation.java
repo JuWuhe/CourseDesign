562e2e1ea8011acab9c8bc185e9f212f4a9cb717
JuWuhe <Insyent@foxmail.com>
2022-06-01 22:03:03 +0800

    b.填写个人资料(未测试)

--- src/main/java/com/course/service/FillInformation.java
+++ src/main/java/com/course/service/FillInformation.java
@@ -1,8 +1,18 @@
 package com.course.service;
 
+import com.course.dao.ScoreMapper;
+import com.course.dao.UserMapper;
+import com.course.pojo.LoginUser;
 import com.course.pojo.PointObject;
+import com.course.pojo.ScoreRecord;
+import com.course.service.score.FillInformationScoreStrategy;
 import com.course.utils.FileUtils;
 import com.course.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+import static com.course.configuration.InterceptorConfig.USER_CONTEXT;
 
 /**
  * @author lixuy
@@ -10,9 +20,21 @@ import com.course.utils.JsonUtils;
  */
 //类名与方法名须与controller层拦截的方法一致
 public class FillInformation {
+    @Autowired
+    private ScoreMapper scoreMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-    public void fillInformation(){
-        System.out.println("+++++fillInformation积分计算方法执行+++++");
+    FillInformationScoreStrategy fillInformationScoreStrategy;
+
+    public int fillInformation(String information){
+        LoginUser user = USER_CONTEXT.get();
+        int i = userMapper.updateInformation(user.getUserId(), information);
+        boolean isFirst = user.getInformation() == null;
+        user.setInformation(information);
+        ScoreRecord scoreRecord = fillInformationScoreStrategy.record(user, Map.of("isFirst", isFirst));
+        if(scoreRecord != null) scoreMapper.insertRecord(scoreRecord);
+        return i;
     }
 
 }
