9d7f8a12cebae0a8724b65c7a9dbc792586385bb
JuWuhe <Insyent@foxmail.com>
2022-06-02 12:30:37 +0800

    a.参加科研招募单元测试

--- src/main/java/com/course/service/ResearchRecruitmentService.java
+++ src/main/java/com/course/service/ResearchRecruitmentService.java
@@ -7,6 +7,7 @@ import com.course.pojo.ScoreRecord;
 import com.course.service.score.ConvertibleScoreStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
@@ -17,22 +18,17 @@ import static com.course.configuration.InterceptorConfig.USER_CONTEXT;
  * Created on 2019-04-11
  */
 //类名与方法名须与controller层拦截的方法一致
+    @Service
 public class ResearchRecruitmentService {
     @Autowired
     private ScoreMapper scoreMapper;
 
     @Autowired
     private EventBus EventBus;
+    @Autowired
     private ConvertibleScoreStrategy convertibleScoreStrategy;
     public void researchRecruitment(){
         System.out.println("+++++researchRecruitment积分计算方法执行+++++");
         EventBus.publishEvent(new ConvertibleScoreEvent(USER_CONTEXT.get(), ConvertibleScoreEvent.ConvertibleScore.ResearchRecruitment));
-    }
-
-    @EventListener
-    public void ConvertibleScore(ConvertibleScoreEvent event){
-        ScoreRecord record = convertibleScoreStrategy.record(event.getLoginUser(), Map.of("score",event.getScore(),"type",event.getTrueType()));
-        if(record == null) return;
-        scoreMapper.insertRecord(record);
     }
 }
