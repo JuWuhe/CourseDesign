65c44d117e3786e88efb65002ab48286c1314d9a
JuWuhe <Insyent@foxmail.com>
2022-06-02 10:34:46 +0800

    b.填写个人资料单元测试

--- src/main/java/com/course/service/FollowUpService.java
+++ src/main/java/com/course/service/FollowUpService.java
@@ -12,15 +12,15 @@ import static com.course.configuration.InterceptorConfig.USER_CONTEXT;
 //类名与方法名须与controller层拦截的方法一致
 @Service
 public class FollowUpService {
-    @Autowired
-    ConvertibleScoreStrategy css;
-
-    public void followUp(){
-        System.out.println("+++++followUp积分计算方法执行+++++");
-        EventBus.publishEvent(new ConvertibleScoreEvent(USER_CONTEXT.get(), ConvertibleScoreEvent.ConvertibleScore.FollowUp));
-        ScoreRecord record = css.record(USER_CONTEXT.get(), Map.of("score",3,"type",6));
-        if(record == null) return;
-        scoreMapper.insertRecord(record);
-    }
+//    @Autowired
+//    ConvertibleScoreStrategy css;
+//
+//    public void followUp(){
+//        System.out.println("+++++followUp积分计算方法执行+++++");
+//        EventBus.publishEvent(new ConvertibleScoreEvent(USER_CONTEXT.get(), ConvertibleScoreEvent.ConvertibleScore.FollowUp));
+//        ScoreRecord record = css.record(USER_CONTEXT.get(), Map.of("score",3,"type",6));
+//        if(record == null) return;
+//        scoreMapper.insertRecord(record);
+//    }
 
 }
