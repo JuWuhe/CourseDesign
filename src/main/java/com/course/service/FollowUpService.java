574099eefbc80aa24392e430deb816946464d563
B19031630陶岳隆 <2293164297@qq.com>
2022-06-02 12:34:58 +0800

    a.门诊随访update

--- src/main/java/com/course/service/FollowUpService.java
+++ src/main/java/com/course/service/FollowUpService.java
@@ -1,10 +1,12 @@
 package com.course.service;
+
 import com.course.event.ConvertibleScoreEvent;
 import com.course.event.EventBus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static com.course.configuration.InterceptorConfig.USER_CONTEXT;
+
 /**
  * @author lixuy
  * Created on 2019-04-11
@@ -13,14 +15,11 @@ import static com.course.configuration.InterceptorConfig.USER_CONTEXT;
 @Service
 public class FollowUpService {
     @Autowired
-    ConvertibleScoreStrategy css;
+    private EventBus EventBus;
 
     public void followUp(){
         System.out.println("+++++followUp积分计算方法执行+++++");
         EventBus.publishEvent(new ConvertibleScoreEvent(USER_CONTEXT.get(), ConvertibleScoreEvent.ConvertibleScore.FollowUp));
-        ScoreRecord record = css.record(USER_CONTEXT.get(), Map.of("score",3,"type",6));
-        if(record == null) return;
-        scoreMapper.insertRecord(record);
     }
 
 }
